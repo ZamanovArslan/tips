6efea4b5fa3434f568eb2eca311572f22f7bc538
lionZamanov <kotleta.first@gmail.com>
2020-02-09 22:03:41 +0300

    temp change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@ CSV.foreach(FILE_PATH, headers: true, header_converters: :symbol) do |row|
   LifeArea.create(value: row[:value], en_value: row[:en_value])
 end
 
+# Add admin
+User.create(role: :admin, email: ENV["ADMIN_EMAIL"], full_name: "Admin", password: ENV["ADMIN_PASSWORD"])
+
+# Add tips to admin
+user = User.find_by(email: ENV["ADMIN_EMAIL"])
+20.times do
+  FactoryGirl.create(:tip, user: user)
+end
--- db/seeds/development/all.seeds.rb
+++
@@ -1,5 +0,0 @@
-# User tips
-user = User.find_by(email: "lionzamanov@gmail.com")
-20.times do
-  FactoryGirl.create(:tip, user: user)
-end
