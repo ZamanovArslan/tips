3ab5c0d146eebed5b01f44ac02e91dfb09618054
LionZam <lionzamanov@gmail.com>
2020-02-13 17:30:30 +0300

    add test

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :company do
     name "MyString"
-    owner nil
   end
 end
--- spec/features/visitor/sign_in_spec.rb
+++ spec/features/visitor/sign_in_spec.rb
@@ -3,12 +3,17 @@ require "rails_helper"
 feature "Sign In" do
   let(:user) { create :user }
   let(:unconfirmed_user) { create :user, :not_confirmed }
+  let(:company) { create :company, name: "flatstack" }
 
   def sign_in(email, password)
     visit new_user_session_path
 
     fill_form(:user, email: email, password: password)
     click_button "Sign in"
+  end
+
+  def company_subdomain
+    new_user_session_url.gsub("www", company.name)
   end
 
   scenario "Visitor signs in with valid credentials" do
@@ -28,5 +33,14 @@ feature "Sign In" do
     sign_in(unconfirmed_user.email, user.password)
 
     expect(page).to have_content("You have to confirm your email address before continuing.")
+  end
+
+  scenario "Visitor can't signs in another subdomain" do
+    visit company_subdomain
+
+    fill_form(:user, email: user.email, password: user.password)
+    click_button "Sign in"
+
+    expect(page).to have_content("Invalid Email or password")
   end
 end
